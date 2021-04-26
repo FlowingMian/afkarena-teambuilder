aee0c6a9656755b2f561540bc06621dc6d06376c
Mian <mian.tram@attestationlegale.fr>
2021-04-26 21:43:04 +0200

    Page ready

---
+++ src/data/signatureItems/index.tsx
@@ -0,0 +1,8 @@
+
+import { SignatureItemsGuide } from '../../model/signatureItems';
+import linker from './signatureItems-linker';
+
+const signatureItemsGuides: Array<SignatureItemsGuide> = [
+  linker
+];
+export default signatureItemsGuides;
